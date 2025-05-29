75daf61221b27e1cd8e6eb8558fd3ce411771811
montesor.shainaa <montesor.shainaa@gmail.com>
2025-05-30 00:38:31 +0800

    Added code for TAHL

--- index.js
+++ index.js
@@ -104,23 +104,3 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const popup = document.getElementById("contactPopup");
-//   const openBtn = document.getElementById("openContactPopup");
-//   const closeBtn = document.getElementById("closePopupBtn");
-
-//   openBtn.addEventListener("click", () => {
-//     popup.style.display = "block";
-//   });
-
-//   closeBtn.addEventListener("click", () => {
-//     popup.style.display = "none";
-//   });
-
-//   window.addEventListener("click", (e) => {
-//     if (e.target === popup) {
-//       popup.style.display = "none";
-//     }
-//   });
-// });
